ccfe38ef60f01a17a0b062ea6ec420a7ba6e23bc
Natali Perebeynos <perebeynos-98@mail.ru>
2026-05-12 17:31:24 +0300

    Урок 22 об + доп задания

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,17 +2,23 @@ const menu = () => {
     const menuBtn = document.querySelector(".menu");
     const menu = document.querySelector("menu");
 
-    const handeleMenu = () => {
-        menu.classList.toggle("active-menu");
+    const toggleMenu = (e) => {
+        if (e.target.closest(".menu")) {
+            menu.classList.toggle("active-menu");
+            return;
+        }
+
+        if (
+            menu.classList.contains("active-menu") &&
+            (e.target.closest(".close-btn") ||
+                e.target.closest("ul li a") ||
+                !e.target.closest("menu"))
+        ) {
+            menu.classList.toggle("active-menu");
+        }
     };
 
-    menuBtn.addEventListener("click", handeleMenu);
-
-    menu.addEventListener("click", (e) => {
-        if (e.target.closest(".close-btn") || e.target.closest("ul li a")) {
-            handeleMenu();
-        }
-    });
+    document.addEventListener("click", toggleMenu);
 };
 
 export default menu;
