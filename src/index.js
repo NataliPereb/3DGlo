9cb2ff60feb95e77a31fc9e07e8f3f63ef8e8c22
Natali Perebeynos <perebeynos-98@mail.ru>
2026-05-12 22:04:19 +0300

    Revert "Урок 23"
    This reverts commit 0e8a84d0a34a2e5b7a8558ed410d5a3497bf46a0.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import modal from "./modules/modal";
 import calc from "./modules/calc";
 import forms from "./modules/forms";
 import tabs from "./modules/tabs";
-import addDots from "./modules/addDots";
-import slider from "./modules/slider";
 
 timer("01 september 2026");
 menu();
@@ -13,5 +11,3 @@ modal();
 calc();
 forms();
 tabs();
-addDots();
-slider();
