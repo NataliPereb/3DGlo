8438e9fb8eb44a6ff01eb87b5e3fa8e4806ed412
Natali Perebeynos <perebeynos-98@mail.ru>
2026-05-13 16:52:04 +0300

    Урок 25

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,10 +1,33 @@
+import { animate } from "./helpers";
+
 const modal = () => {
     const buttons = document.querySelectorAll(".popup-btn");
     const modal = document.querySelector(".popup");
+    const popupContent = document.querySelector(".popup-content");
+
+    const startAnimation = () => {
+        popupContent.style.left = "0%";
+
+        modal.style.display = "block";
+
+        setTimeout(
+            animate({
+                duration: 500,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
+                    popupContent.style.left = 38 * progress + "%";
+                },
+            }),
+            1500,
+        );
+    };
 
     buttons.forEach((btn) => {
         btn.addEventListener("click", () => {
             modal.style.display = "block";
+            startAnimation();
         });
     });
 
@@ -14,6 +37,7 @@ const modal = () => {
             e.target.classList.contains("popup-close")
         ) {
             modal.style.display = "none";
+            popupContent.style.left = "0%";
         }
     });
 };
